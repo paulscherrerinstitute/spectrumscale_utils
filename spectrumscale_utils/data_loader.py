b62b5ca22ae1f77d9757cea70152ded2a200080d
Leonardo Sala <leonardo.sala@psi.ch>
2018-10-01 15:19:36 +0200

    minor

--- spectrumscale_utils/data_loader.py
+++ spectrumscale_utils/data_loader.py
@@ -25,7 +25,7 @@ def get_data_from_mmrepquota(f, groupby="filesetname"):
 
     df.drop(["mmrepquota", "Unnamed: 1", "HEADER", "reserved", "reserved.1"], axis=1, inplace=True)
     df.drop(df[df.version == "version"].index, inplace=True)
-    df["blockUsage"] = pd.to_numeric(df["blockUsage"]) / 1e9
+    df["blockUsage"] = pd.to_numeric(df["blockUsage"]) / 1e6
     filesystems = df[groupby].unique()
     data_usage_group = {}
 
@@ -79,11 +79,16 @@ def get_timeseries_from_mmrepquota(datadir, quantity="blockUsage", groupby="file
                 # skip root fileset, as it has the same name 
                 if k == "root":
                     continue
-                if k not in dfs:
-                    dfs[k] = pd.DataFrame()
+                
                 cname = d + " %d:00:00" % int(hour)
-                dfs[k][cname] = df_t[k][quantity]
-    
+                serie = df_t[k][quantity]
+                serie.name = cname
+                if k not in dfs:
+                    dfs[k] = serie 
+                else:
+                    dfs[k] = pd.concat([dfs[k], serie], axis=1, sort=True)
+                #print(k, ",", dfs[k][cname].sum(), df_t[k][quantity].sum())
+
             points += 1
     
             if points_per_dir <= points:
@@ -92,12 +97,13 @@ def get_timeseries_from_mmrepquota(datadir, quantity="blockUsage", groupby="file
     for k in dfs.keys():
         dfs[k].columns = pd.to_datetime(dfs[k].columns)
         dfs[k] = dfs[k].T
+        dfs[k].sort_index(inplace=True)
     
     print("Read %d points" % points)
     return dfs
 
 
-def get_timeseries_from_policy(f):
+def get_timeseries_from_policy(f, header, index_date="CREATION", drop_separators=True):
     """Get a pandas DataFrame from a policy scan such as:
 
     RULE 'listall' list 'all-files'  
@@ -115,13 +121,33 @@ def get_timeseries_from_policy(f):
     """
 
     headers = ["Inode number", "gen number", "Snapshot ID", ]
-    headers += ["kb_allocated", "sep1", "filesize", "sep2", "user_id", "sep3", "fileset_name", "sep4", "creation_date", "creation_time"]
-    headers += ["Seperator", "Filename"]
+    seps = []
+    i = 0
+    for i, h in enumerate(header):
+        headers.append(h)
+        if h.find("DATE") != -1:
+            headers.append(h.replace("DATE", "TIME"))
+        seps.append("sep%d" % i)
+        headers.append("sep%d" % i)
+
+    #headers += ["kb_allocated", "sep1", "filesize", "sep2", "user_id", "sep3", "fileset_name", "sep4", "creation_date", "creation_time"]
+    headers += ["Filename"]
 
     df = pd.read_csv(f, sep=r"\s+", names=headers, )
 
-    df["date"] = df["creation_date"] + " " + df["creation_time"]
+    df["date"] = df[index_date + "_DATE"] + " " + df[index_date + "_TIME"]
     df["date"] = pd.to_datetime(df["date"])
     df2 = df.set_index("date")
     df2.sort_index(inplace=True)
+    if drop_separators:
+        for sep in seps:
+            df2.drop(sep, inplace=True, axis=1)
     return df2
+
+
+def get_data_from_iohist(f, verbose_iohist=False):
+    header = ["RW", "buf type", "disk:sec", "nSec", "time_ms", "type", "NSD ID", "NSD node"]
+    if verbose_iohist:
+        header += ["info1", "info2", "context", "thread"]
+    df = pd.read_csv(f, skiprows=7, sep=r"\s*", names=header, engine="python")
+    return df
